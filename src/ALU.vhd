a5cd58f8802f685431edc960ccfebdcbe25b58b7
elijahB5 <c27elijah.butler@afacademy.af.edu>
2025-05-05 20:46:46 -0600

    first complete

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -32,16 +32,74 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ALU is
-    Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
-           i_B : in STD_LOGIC_VECTOR (7 downto 0);
-           i_op : in STD_LOGIC_VECTOR (2 downto 0);
-           o_result : out STD_LOGIC_VECTOR (7 downto 0);
-           o_flags : out STD_LOGIC_VECTOR (3 downto 0));
+    port (
+        i_A       : in  std_logic_vector(7 downto 0);
+        i_B       : in  std_logic_vector(7 downto 0);
+        i_op      : in  std_logic_vector(2 downto 0);
+        o_result  : out std_logic_vector(7 downto 0);
+        o_flags   : out std_logic_vector(3 downto 0)  -- ZNCV: Zero, Negative, Carry, Overflow
+    );
 end ALU;
-
-architecture Behavioral of ALU is
-
-begin
-
-
-end Behavioral;
+architecture behavioral of ALU is
+    component ripple_adder is 
+    Port( A : in STD_LOGIC_VECTOR (3 downto 0);
+          B : in STD_LOGIC_VECTOR (3 downto 0);
+          Cin : in STD_LOGIC;
+          S : out STD_LOGIC_VECTOR (3 downto 0);
+          Cout : out STD_LOGIC
+          );
+    end component ripple_adder;
+    signal A_low, A_high    :   std_logic_vector(3 downto 0);
+    signal B_low, B_high    :   std_logic_vector(3 downto 0);
+    signal B_mod            :   std_logic_vector(7 downto 0);
+    signal sum_low, sum_high :  std_logic_vector(3 downto 0);
+    signal carry_low : STD_LOGIC;
+    signal carry_high : STD_LOGIC;
+    signal alu_result : STD_LOGIC_VECTOR(7 downto 0);
+    signal Cin       : STD_LOGIC;
+    signal sum_final  : STD_LOGIC_VECTOR(7 downto 0);
+    signal xnor_s     : std_logic;
+    signal xor_s      : std_logic;
+    signal alu_not    : std_logic;
+    signal x_and      : std_logic;
+begin 
+    A_high <= i_A(7 downto 4);
+    A_low  <= i_A(3 downto 0);
+    B_mod <= i_B when i_op /= "001" else (not i_B);
+    B_high <= B_mod(7 downto 4);
+    B_low <= B_mod(3 downto 0);
+    Cin <= '1' when i_op = "001" else '0';
+    ripple_adder_1: ripple_adder
+        port map(
+            A => A_low,
+            B => B_low,
+            Cin => Cin,
+            S => sum_low,
+            Cout => carry_low
+            );
+     ripple_adder_2: ripple_adder
+         port map(
+            A => A_high,
+            B => B_high,
+            Cin => carry_low,
+            S => sum_high,
+            Cout => carry_high
+            );
+     sum_final(7 downto 4) <= sum_high;
+     sum_final(3 downto 0) <= sum_low;
+     with i_op select
+     alu_result <= sum_final when "000",
+                   sum_final when "001",
+                   (B_mod and i_A) when "010",
+                   (B_mod or i_A) when "011",
+                   (others => '0') when others;
+      o_result <= alu_result;
+      o_flags(3) <= alu_result(7);
+      o_flags(2) <= '1' when alu_result = "00000000" else '0';
+      o_flags(1) <= carry_high and (not i_op(1));
+      alu_not <= not i_op(1);
+      xnor_s <= not (i_A(7) xor i_B(7) xor i_op(0));
+      xor_s <= i_A(7) xor alu_result(7);
+      x_and <= xnor_s and xor_s;
+      o_flags(0) <= x_and and alu_not;
+end behavioral;
--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -17,29 +17,64 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
+ 
+ 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+ 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
-
+ 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
+ 
 entity controller_fsm is
     Port ( i_reset : in STD_LOGIC;
            i_adv : in STD_LOGIC;
            o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
 end controller_fsm;
-
+ 
 architecture FSM of controller_fsm is
-
+    type state_t is (S0, S1, S2, S3);
+    signal state, n_state: state_t;
 begin
-
-
+    process(i_adv, i_reset)
+    begin
+        if i_reset = '1' then
+            state <= S0;
+        elsif rising_edge(i_adv) then
+            state <= n_state;
+        end if;
+    end process;
+  process(state)
+    begin
+        case state is
+        when S0 =>
+            n_state <= S1;
+        when S1 =>
+            n_state <= S2;
+        when S2 => 
+            n_state <= S3;
+        when S3 =>
+            n_state <= S0;
+        end case;
+    end process;
+  process(state)
+    begin
+        case state is 
+        when S0 => 
+            o_cycle<="0001";
+        when S1 =>
+            o_cycle <= "0010";
+        when S2 =>
+            o_cycle <= "0100";
+        when S3 =>
+            o_cycle <= "1000";
+    end case;
+  end process;
+ 
+ 
 end FSM;
