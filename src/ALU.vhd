9a470818d389aa318abf3386506b933b3f48371e
elijahB5 <c27elijah.butler@afacademy.af.edu>
2025-05-05 20:58:20 -0600

    retry from push failure?

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -17,20 +17,9 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 entity ALU is
     port (
         i_A       : in  std_logic_vector(7 downto 0);
